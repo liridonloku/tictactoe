25123c3228f92566f8a65779db05286a52e7dca7
Liridon Loku <liridonloku@gmail.com>
2021-09-05 16:29:30 +0200

    Add game function

--- app.js
+++ app.js
@@ -7,26 +7,39 @@ const player2 = playerFactory('player2', 'O');
 
 const gameBoard = (() => {
     const board = [
-        ['O', 'X', 'O'],
-        ['O', 'X', 'O'],
-        ['X', 'O', 'X']
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', '']
     ];
+    let turn = 'player1';
+    const play = (e) => {
+        let symbol;
+        if(turn === player1.name || turn === ''){
+            symbol = player1.symbol;
+            turn = player2.name;
+        }
+        else{
+            symbol = player2.symbol;
+            turn = player1.name;
+        }
+        if(e.target.textContent === ''){
+            e.target.textContent = symbol;
+        }
+    }
     return{
-        board
+        board, play
     };
 })();
 
-const displayController = (() => {
+const game = (() => {
     const board = document.getElementById('board');
     board.replaceChildren();
     for(let i=0; i<=2; i++){
         for(let j=0; j<=2; j++){
             const cell = document.createElement('div');
-            const symbol = document.createElement('h1');
-            symbol.textContent = gameBoard.board[i][j];
-            cell.appendChild(symbol);
+            cell.textContent = gameBoard.board[i][j];
             board.appendChild(cell);
+            cell.addEventListener('click', gameBoard.play);
         }
     }
-    return { board };
 })();
