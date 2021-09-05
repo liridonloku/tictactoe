5dc5dc371a796d8dafba32aa7ee22b7a1b046d4a
Liridon Loku <liridonloku@gmail.com>
2021-09-05 18:19:25 +0200

    Add result display and restart button

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ const playerFactory = (name, symbol) => {
 };
 
 const players = (() => {
-    const player1 = playerFactory('player1', 'X');
-    const player2 = playerFactory('player2', 'O');
-    const checkGameEnd = () =>{
+    const firstPlayer = document.getElementById('player1').value;
+    const secondPlayer = document.getElementById('player2').value;
+    const player1 = playerFactory(firstPlayer, 'X');
+    const player2 = playerFactory(secondPlayer, 'O');
+    const checkGameEnd = () => {
+        const resultContainer = document.querySelector('.result');
+        const result = document.getElementById('result');
         const board = gameBoard.board;
         let a = board[0][0];
         let b = board[0][1];
@@ -18,15 +22,18 @@ const players = (() => {
         let i = board[2][2];
         //conditions for player1 winning
         if(a===b&&a===c&&a==='X'||d===e&&d===f&&d==='X'||g===h&&g===i&&g==='X'||a===d&&a===g&&a==='X'||b===e&&b===h&&b==='X'||c===f&&c===i&&c==='X'||a===e&&a===i&&a==='X'||c===e&&c===g&&c==='X'){
-            console.log('Player 1 Wins');
+            result.textContent = `Game over. ${player1.name} wins!`;
+            resultContainer.style.display = 'flex';
         }
         //conditions for player2 winning
         else if(a===b&&a===c&&a==='O'||d===e&&d===f&&d==='O'||g===h&&g===i&&g==='O'||a===d&&a===g&&a==='O'||b===e&&b===h&&b==='O'||c===f&&c===i&&c==='O'||a===e&&a===i&&a==='O'||c===e&&c===g&&c==='O'){
-            console.log('Player 2 Wins');
+            result.textContent = `Game over. ${player2.name} wins!`;
+            resultContainer.style.display = 'flex';
         }
         //condition for tie
         else if(!(a===''||b===''||c===''||d===''||e===''||f===''||g===''||h===''||i==='')){
-            console.log('Tie');
+            result.textContent = `Game over. It's a tie!`;
+            resultContainer.style.display = 'flex';
         }
     }
     return {
@@ -36,7 +43,7 @@ const players = (() => {
 
 
 const gameBoard = (() => {
-    const board = [
+    let board = [
         ['', '', ''],
         ['', '', ''],
         ['', '', '']
@@ -58,8 +65,23 @@ const gameBoard = (() => {
         }
         players.checkGameEnd();
     }
+    const changeTurn = () =>{
+        if (turn=== players.player1.name){
+            turn = players.player2.name;
+        }
+        else {
+            turn = players.player1.name;
+        }
+    }
+    const clearBoard = () =>{
+        for(let i=0;i<3;i++){
+            for(let j=0;j<3;j++){
+                board[i][j] = '';
+            }
+        }
+    }
     return{
-        board, play
+        board, play, clearBoard, changeTurn
     };
 })();
 
@@ -76,4 +98,16 @@ const game = (() => {
             cell.addEventListener('click', gameBoard.play);
         }
     }
-})();
+});
+
+document.getElementById('start').addEventListener('click', () =>{
+    document.querySelector('.settings').style.display = 'none';
+    gameBoard.changeTurn();
+    gameBoard.clearBoard();
+    game();
+});
+
+document.getElementById('restart').addEventListener('click', () => {
+    document.querySelector('.result').style.display = 'none';
+    document.querySelector('.settings').style.display = 'flex';
+});
